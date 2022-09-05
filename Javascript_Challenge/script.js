c26caf3bb1cd2f9e0327ea92fc23e05440f0c415
lanniiee <lav250397@hotmail.com>
2022-09-05 15:47:57 +0100

    added new JS folder for Challenge

---
+++ Javascript_Challenge/script.js
@@ -0,0 +1,18 @@
+// 1. Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+const isNumberPositive = (num) => {
+    if (num < 0) {
+        return false;
+    } else return true;
+}
+
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(10));
+
+// 2. Write a function that takes a number of days and converts it into an age.
+const convertDaysToAge = (days) => {
+    let age = days/365;
+    console.log(age); 
+}
+
+convertDaysToAge(3650);
+convertDaysToAge(6570);
